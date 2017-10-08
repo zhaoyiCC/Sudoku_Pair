22b323046b77e52a977b1e8b16d1ecc327822d9c
ohazyi_ACT <291045048@qq.com>
2017-10-08 17:19:14 +0800

    Step1 done: implement step 1

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -2,40 +2,12 @@
 //  Created by ohazyi on 2017/10/1.
 //  Copyright © 2017 ohazyi. All rights reserved.
 
-#include <sys/timeb.h>
-#include <time.h>
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <vector>
-#include <fstream>
+#include "sudoku.h"
+
 struct timeb tb;
-
-#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
-inline int belonging(int x, int y) { return x / 3 * 3 + y / 3 + 1; }
-inline int Position(int x, int y) { return 9 * x + y; }
-// using namespace std;
-using std::cin;
-using std::cout;
-using std::string;
-using std::endl;
-
-const int N = 11;
-const int M = 83;
-int tot, x[M], a[M][N], a_backup[M][M][N], aaa[M];
-bool hasAnswer = false, debug = false, debug_time = false;
-bool modifyElement(int pos, int r), deleteElement(int pos, int r);
-FILE *out;
-
-int value, cnt, n, m, len = 0;  // value代表要输出的矩阵的个数
-char ch;
-bool vis_col[N][N], vis_row[N][N], vis_magic[N][N];
-
-inline bool find(int x, int y, int z) {
-	return (!vis_row[x][z] && !vis_col[y][z] && !vis_magic[belonging(x, y)][z]);
-}
-
+bool debug = false, debug_time = true, u_flag = false;
+int val = 0, r1, r2, pos_r;
+char c[110];
 string getTime() {  // 得到当前时间的ms的时间
 	time_t timep;
 	time(&timep);
@@ -51,183 +23,155 @@ void printTime(string str) {  // 得到当前时间
 	ftime(&tb);
 	printf(".%03d\n", tb.millitm);
 }
-bool deleteElement(int pos, int r) {
-	int i;
-	if (a[pos][r] == 1)  // 代表之前已经不让填了
-		return true;
-	a[pos][r] = 1;
-	if (++a[pos][0] == 9)  // 代表这个格子没有值可以填了
-		return false;
-	if (a[pos][0] == 8) {
-		for (i = 1; i <= 9; ++i)
-			if (a[pos][i] == 0)  // 找到是谁可以填
-				break;
-		if (!modifyElement(pos, i))
-			return false;
-	}
-	return true;
-}
-bool modifyElement(int pos, int r) {
-	int p = pos / 9;  // 行号
-	int q = pos % 9;  // 列号
-	int h;
-	x[pos] = r;
-	rep(i, 1, 9)
-		a[pos][i] = 1;
-	a[pos][0] = 8;  // 代表只有一个元素可以填
-	a[pos][r] = 0;  // 那就是r
-	rep(j, 0, 8) {  // 增加同行的其它格子的限制
-		h = Position(p, j);
-		if (h != pos && !deleteElement(h, r))
-			return false;
-	}
-	rep(i, 0, 8) {  // 增加同列的其它格子的限制
-		h = Position(i, q);
-		if (h != pos && !deleteElement(h, r))
-			return false;
-	}
-	rep(i, 0, 2)  // 增加同九宫格的其它格子的限制
-		rep(j, 0, 2) {
-		h = (3 * (p / 3) + i) * 9 + (3 * (q / 3) + j);
-		if (h != pos && !deleteElement(h, r))
-			return false;
-	}
-	return true;
-}
-void out_file(int *s) {
-	char ans[300];
-	int len = 0;
-	rep(i, 0, 80) {
-		ans[len++] = s[i];
-		ans[len++] = (((i + 1) % 9) == 0) ? '\n' : ' ';
-		if (debug) {
-			printf("%c ", s[i]);
-			if ((i + 1) % 9 == 0)
-				printf("\n");
-		}
-	}
-	if (debug)
-		printf("\n");
 
-	ans[len++] = '\n';
-	ans[len] = '\0';
-	// ans[len] = '\0';  // fputs(ans, out); // fputs("\n", out);
-	fwrite(ans, 1, strlen(ans), out);
-	// ans[len] = '\0'; //fprintf(out, "%s\n", ans);
-}
-void dfs(int k) {
-	int mx = -1, mxi;
-	rep(i, 0, 80) {
-		if (a[i][0]<8 && a[i][0]>mx) {
-			mx = a[i][0];
-			mxi = i;
-		}
-	}
-	if (mx == -1) {
-		rep(i, 0, 80) {
-			x[i] += '0';
-		}
-		out_file(x);
-		hasAnswer = true;
-		return;
-	}
-	memcpy(a_backup[k], a, sizeof(a));
-	rep(i, 1, 9) {
-		if (a[mxi][i] == 0) {
-			if (modifyElement(mxi, i))
-				dfs(k + 1);
-			memcpy(a, a_backup[k], sizeof(a));
-		}
-		if (hasAnswer)
-			return;
-	}
-}
-void dfs2(int k) {
-	if (hasAnswer)
-		return;
-	int x, y, h;
-	x = k / 9;
-	y = k % 9;
-	h = belonging(x, y);
-	if (k > 80) {
-		out_file(aaa);
-		if (++cnt >= value) {
-			hasAnswer = true;
-			// exit(0);
-		}
-		return;
-	}
-	rep(i, 1, 9) {
-		if (find(x, y, i)) {
-			aaa[k] = i + '0';
-
-			vis_row[x][i] = vis_col[y][i] = vis_magic[h][i] = true;
-			dfs2(k + 1);
-			vis_row[x][i] = vis_col[y][i] = vis_magic[h][i] = false;
-		}
-	}
-}
 bool calc(char* s) {
-	value = 0;
+	val = 0;
 	for (int i = 0; s[i]; ++i) {
 		if (s[i] < '0' || s[i] > '9')
 			return false;
-		value = value * 10 + s[i] - '0';
+		val = val * 10 + s[i] - '0';
 	}
+	cout << "val=" << val << endl;
 	return true;
 }
 void exit_invalid(string s) {
 	cout << s << endl;
 	exit(0);
 }
+
 int main(int argc, char *argv[]) {
-	if (argc != 3)
-		exit_invalid("Too many or too few arguments!");
-	if (strlen(argv[1]) < 2)
+	srand((int)time(0));
+
+	Core s;
+	/*if (argc != 3)
+		exit_invalid("Too many or too few arguments!");*/
+	if (strlen(argv[1]) != 2)
 		exit_invalid("Invalid arguments!");
-	out = fopen("sudoku.txt", "w");    // freopen("sudoku.txt", "w", stdout);
-	if (argv[1][1] == 'c') {
-		value = 1;  // value = atoi(argv[2])
-		if (!calc(argv[2])) {
-			puts("Invalid Command -c Number");
-			return 0;
-		}
-		if (value<1 || value>1000000) {
-			puts("Invalid -c number");
-			return 0;
-		}
+	s.out = fopen("sudoku.txt", "w");    // freopen("sudoku.txt", "w", stdout);
+	s.debug = debug;
+	if (debug_time) printTime("起始时间");
+	if (strcmp(argv[1],"-c")==0) {
+		val = 1;  // value = atoi(argv[2])
+		if (!calc(argv[2]))
+			exit_invalid("Invalid Command -c Number");
+		if (val < 1 || val>1000000) 
+			exit_invalid("Invalid -c number");
 
-		memset(vis_magic, false, sizeof(vis_magic));
-		memset(vis_col, false, sizeof(vis_col));
-		memset(vis_row, false, sizeof(vis_row));
-		cnt = 0;
-		vis_row[0][1] = vis_col[0][1] = vis_magic[belonging(0, 0)][1] = true;
-		aaa[0] = 1 + '0';
-		if (debug_time) printTime("起始时间");
-		// value = 1000000;
-		dfs2(1);
-		if (debug_time) printTime("结束时间");
+		s.init_gen(val, 1);
 	}
-	else if (argv[1][1] == 's') {
+	else if (strcmp(argv[1], "-s") == 0) {
 		freopen(argv[2], "r", stdin);  // freopen("puzzlefile.txt", "r", stdin);
+		
+		s.init_sol();
+	}
+	else {
+		vector<string> cmd_type = { "-n", "-m", "-r", "-u" };
+		string str(argv[1]);
+		int type = -1;
+		bool flag_arg;
+		string arg_s, r_1, r_2;
+		map<string, int> mp;
+		for (int i_arg = 1; i_arg < argc; i_arg++) {
 
-		if (debug_time) printTime("起始时间");
-		while (scanf("%c", &ch) > 0) {
-			if (ch < '0' || ch > '9')
+			flag_arg = false;
+			for (auto sss : cmd_type) {
+				if (sss == argv[i_arg]) {
+					flag_arg = true;
+					arg_s = sss;
+					break;
+				}
+			}
+			if (!flag_arg) {
+				exit_invalid("Invalid Command - argument");
+			}
+			if (arg_s == "-u") {//-u -n 10
+				u_flag = true;
 				continue;
-			x[tot++] = ch - '0';
-			if (tot == 81) {
-				rep(i, 0, 80)
-					if (x[i] > 0)
-						modifyElement(i, x[i]);
+			}
+			if ((++i_arg) == argc) {//-n -u 10
+				exit_invalid("Lack number");
+			}
+			
+			if (arg_s == "-r") {
+				flag_arg = false;
+				rep (j, 0, (int)strlen(argv[i_arg]) - 1) {
+					if (argv[i_arg][j] == '~') {
+						flag_arg = true;
+						pos_r = j;
+						c[j] = '\0';
+						break;
+					}
+					c[j] = argv[i_arg][j];
+				}
+				if (!flag_arg)
+					exit_invalid("Invalid -r argument");
 
-				dfs(1);
-				tot = 0;
-				hasAnswer = false;
-				memset(a, 0, sizeof(a));
+				if (!calc(c))
+					exit_invalid("Invalid -r number1");
+				r1 = val;
+				memset(c, 0, sizeof(c));
+				rep(j, pos_r + 1, (int)strlen(argv[i_arg]))
+					c[j - pos_r - 1] = argv[i_arg][j];
+				if (!calc(c))
+					exit_invalid("Invalid -r number2");
+				r2 = val;
+				if (r1 < 20 || r2 < 20 || r1 > 55 || r2 > 55 || r1 > r2) 
+					exit_invalid("-r number should be in [20, 55] and r1<=r2");
+				mp[arg_s] = r1*100+r2;
+				continue ;
+			}
+			if (calc(argv[i_arg])) {
+				if (arg_s == "-n" && (val<1 || val>10000))
+					exit_invalid("Invalid -n number");
+				if (arg_s == "-m" && (val<1 || val>3))
+					exit_invalid("Invalid -m number");
+				if (arg_s == "-r" && (val<20 || val>55))
+					exit_invalid("Invalid -r number");
+				if (mp.count(arg_s) > 0)
+					exit_invalid("Repeated parameter!");
+				mp[arg_s] = val;
+			}
+			else {
+				exit_invalid("Invalid Command Number");
+			}
+
+		}
+
+		for (auto sss : cmd_type) {
+			if (sss == "-u")
+				continue;
+			cout << sss << " : " << mp.count(sss) << "    ";
+			if (mp.count(sss) > 0)
+				cout << mp[sss];
+			cout << endl;
+		}
+
+		if (mp.count("-m") > 0) {
+			cout << u_flag << "u——flag" << endl;
+			if (u_flag)
+				exit_invalid("-u can not be with -m");
+			if (mp.size() > 2)
+				exit_invalid("Too many arguments!");
+			if (mp.count("-n") == 0)
+				exit_invalid("-m must be with -n");
+			s.init_gen(10000, 0);//mp["-n"]*5
+			s.generate(mp["-n"], mp["-m"], ans_new);
+			rep(i, 1, mp["-n"]){
+				s.out_file(ans_new[i]);
+			}
+		}else
+		if (mp.count("-r") > 0) {  //
+			if (mp.size() > 2)
+				exit_invalid("Too many arguments!");
+			if (mp.count("-n") == 0)
+				exit_invalid("-m must be with -n");
+			s.init_gen(10000, 0);
+			s.generate(mp["-n"], r1, r2, u_flag, ans_new);
+			rep(i, 1, mp["-n"]) {
+				s.out_file(ans_new[i]);
 			}
 		}
-		if (debug_time) printTime("结束时间");
 	}
+	if (debug_time) printTime("结束时间");
 	return 0;
 }
---
+++ sudoku/sudoku.h
@@ -0,0 +1,467 @@
+#include "headers.h"
+
+
+#define random(x) (rand()%x)
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+inline int belonging(int x, int y) { return x / 3 * 3 + y / 3 + 1; }
+inline int Position(int x, int y) { return 9 * x + y; }
+using namespace std;
+const int N = 11;
+const int M = 83;
+
+int ans_all_cnt = 0, ans_all[1000010][M], ans_new[50010][M];
+
+/*
+
+在个人项目上增量修改，实现void generate(int number, int mode, int[][] result)接口，对于输入的数独游戏的需求数量和难度等级，通过result返回生成的数独游戏的集合。
+对这一generate接口进行测试，把单元测试代码Push到Github上(注意避免把单元测试的结果Push到Github上)。
+实现void generate(int number, int lower, int upper, bool unique, int[][] result)接口，生成number个空白数下界为lower，上界为upper的数独游戏，并且如果unique为真，则生成的数独游戏的解必须唯一，计算的结果存储在result中。
+对这一generate接口进行测试，把单元测试代码Push到Github上(注意避免把单元测试的结果Push到Github上)。
+实现bool solve(int[] puzzle, int[] solution)接口，对于输入的数独题目puzzle（数独矩阵按行展开为一维数组），返回一个bool值表示是否有解。如果有解，则将一个可行解存储在solution中（同样表示为数独矩阵的一维数组展开）。
+对solve接口进行测试，把单元测试代码Push到Github上(注意避免把单元测试的结果Push到Github上)。
+设计其他部分的接口，按照设计好的接口在个人项目基础上增量修改，同样把单元测试代码Push到Github上。
+在完成这一阶段的任务之后，使用git tag step1标记第一阶段已经完成，并在Push到Github上时使用--tags参数把tag也推送到Github，例如git push origin --tags。
+
+*/
+
+
+class Core {
+public:
+	int tot, x[M], a[M][N], a_backup[M][M][N], aaa[M];
+	bool debug;
+	int hasAnswer = 0;
+	//bool modifyElement(int pos, int r), deleteElement(int pos, int r);
+	FILE *out;
+
+	int value, cnt, n, m, len = 0;  // value代表要输出的矩阵的个数
+	char ch;
+	bool vis_col[N][N], vis_row[N][N], vis_magic[N][N];
+
+	int num_0, id, game[M];
+	bool isEmpty[M];
+	double p_exist;
+
+	bool find(int x, int y, int z);
+	void init_gen(int val, int type);
+	void init_sol();
+	void trans(int a[81]);
+	bool isEquivalent(int a[81], int b[81]);
+	bool deleteElement(int pos, int r);
+	bool modifyElement(int pos, int r);
+	void out_file(int *s);
+	void dfs1(int k, int type);
+	void dfs2(int k, int type);
+	int freedom(int a[M]);
+	double getRandData(int min, int max);
+	void dfs3(int k, int tot);
+	int work3(int num_0_t, int id_t, double p_t);
+
+	void generate(int number, int mode, int result[][M]);
+	void generate(int number, int lower, int upper, bool unique, int result[][M]);
+	bool solve(int puzzle[M], int solution[M]);
+
+	void generate_single(int number, int from, int ran, int dow, int upd, bool unique, int result[][M]);
+	int solve_unique(int tmp[M]);
+
+	int ans_first[M];
+};
+
+
+
+inline int Core::solve_unique(int tmp[M]) {
+	hasAnswer = 0;//false
+	memcpy(x, tmp, sizeof(x));
+	
+	memset(a, 0, sizeof(a));
+	rep(i, 0, 80) {
+		if (x[i] > 0)
+			modifyElement(i, x[i]);
+	}
+	dfs2(1, 0);
+	return hasAnswer;
+}
+
+inline void Core::generate_single(int number, int from, int ran, int dow, int upd, bool unique, int result[][M]) {
+	int first, fre, cnt_f = 0, id = 1, uni;
+	while (cnt_f < number) {
+		if (debug)
+			cout << "----------------------" << endl;
+		first = random(ran) + from;
+		if (debug)
+			cout << "空白个数：" << first << endl;
+
+		fre = work3(first, id, 1.0*first / 81.0);
+		if (fre >= dow && fre <= upd) {
+			if (unique) {
+				uni = solve_unique(game);
+				if (uni > 1) {
+					if (debug)
+						cout << "^^^^解不唯一  " <<id<< endl;
+					//id++;
+					continue;
+				}
+			}
+
+			//cnt_f++;
+			memcpy(result[++cnt_f], game, sizeof(game));
+		}
+		id++;
+		if (debug)
+			cout << "----------------------" <<id << endl;
+	}
+	cout << "做到了第几个数独: " << id << " 产生数独数： " << cnt_f << endl;
+}
+
+inline void Core::generate(int number, int mode, int result[][M]) {
+	
+	switch(mode) {
+		case 1:
+			generate_single(number, 40, 10, 0, 650, false, result);
+			break;
+		case 2:
+			generate_single(number, 46, 15, 651, 999, false, result);
+			break;
+		case 3:
+			generate_single(number, 56, 10, 1000, 9999, false, result);
+			break;
+		default:
+			cout << "Invalid: -m mode number should be 1 or 2 or 3" << endl;
+			return ;
+	}
+}
+
+inline void Core::generate(int number, int lower, int upper, bool unique, int result[][M]) {
+
+	//generate_single(int number, int from, int ran, int dow, int upd, bool unique, int result[][M])
+
+
+	generate_single(number, lower, upper-lower+1, 0, 9999, unique, result);
+}
+
+inline bool Core::solve(int puzzle[M], int solution[M]) {
+	int sol = solve_unique(puzzle);
+	if (sol >= 1) {
+
+		//cout << "!!!!" <<sizeof(int)<<" "<<sizeof(int *)<<" "<< sizeof(solution) << " *** " << sizeof(ans_first) << endl;
+		memcpy(solution, ans_first, sizeof(ans_first));
+		return true;
+	}
+	return false;
+}
+
+inline bool Core::find(int x, int y, int z) {
+	return (!vis_row[x][z] && !vis_col[y][z] && !vis_magic[belonging(x, y)][z]);
+}
+
+
+
+inline void Core::init_gen(int val, int type) {
+	memset(vis_magic, false, sizeof(vis_magic));
+	memset(vis_col, false, sizeof(vis_col));
+	memset(vis_row, false, sizeof(vis_row));
+
+
+	int sta = random(9) + 1;
+	cnt = 0;
+	vis_row[0][sta] = vis_col[0][sta] = vis_magic[belonging(0, 0)][sta] = true;
+	aaa[0] = sta + '0';
+
+	/*cnt = 0;
+	vis_row[0][1] = vis_col[0][1] = vis_magic[belonging(0, 0)][1] = true;
+	aaa[0] = 1 + '0';*/
+
+	//value = 1000000;
+	value = val;
+	hasAnswer = 0;
+	dfs1(1, type);
+
+	//int lll[11][M];
+	//generate(10, 1, lll);
+
+	//generate(1, 20, 60, true, lll);
+	//int ppp[M] = {
+	//	4, 0, 0, 0, 0, 0, 8, 0, 5,
+	//	0, 3, 0, 0, 0, 0, 0, 0, 0,
+	//	0, 0, 0, 7, 0, 0, 0, 0, 0,
+	//	0, 2, 0, 0, 0, 0, 0, 6, 0,
+	//	0, 0, 0, 0, 8, 0, 4, 0, 0,
+	//	0, 0, 0, 0, 1, 0, 0, 0, 0,
+	//	0, 0, 0, 6, 0, 3, 0, 7, 0,
+	//	5, 0, 0, 2, 0, 0, 0, 0, 0,
+	//	1, 0, 4, 0, 0, 0, 0, 0, 0
+	//};
+
+	//int kkk[M];
+	//bool zzyy = solve(ppp, kkk);
+	////cout << "@@@@@@@@@@@@@@@@@" << endl;
+	//rep(i, 1, 1) {
+	//	rep(j, 0, 80) {
+	//		//cout << lll[i][j] << " ";
+	//		cout << kkk[j] << " ";
+	//		if ((j + 1) % 9 == 0)
+	//			cout << endl;
+	//	}
+	//	cout << endl;
+	//}
+}
+
+
+
+inline void Core::init_sol() {
+	while (scanf("%c", &ch) > 0) {
+		if (ch < '0' || ch > '9')
+			continue;
+		x[tot++] = ch - '0';
+		if (tot == 81) {
+			rep(i, 0, 80)
+				if (x[i] > 0)
+					modifyElement(i, x[i]);
+
+			dfs2(1, 1);
+			tot = 0;
+			hasAnswer = 0;//false
+			memset(a, 0, sizeof(a));
+		}
+	}
+}
+
+
+inline void Core::trans(int a[81]) {
+	int X[10];
+	int n = 9;
+	for (int i = 0; i < n; i++)
+		X[a[i]] = i + 1;
+	for (int i = 0; i < n*n; i++)
+		a[i] = X[a[i]];
+}
+inline bool Core::isEquivalent(int a[81], int b[81]) {
+	int n = 9;
+	int c[81], d[81];
+	memcpy(c, a, sizeof(c));
+	memcpy(d, b, sizeof(d));
+	trans(c);
+	trans(d);
+	for (int i = 0; i < n*n; i++)
+		if (c[i] != d[i])
+			return false;
+	return true;
+}
+
+inline bool Core::deleteElement(int pos, int r) {
+	int i;
+	if (a[pos][r] == 1)  // 代表之前已经不让填了
+		return true;
+	a[pos][r] = 1;
+	if (++a[pos][0] == 9)  // 代表这个格子没有值可以填了
+		return false;
+	if (a[pos][0] == 8) {
+		for (i = 1; i <= 9; ++i)
+			if (a[pos][i] == 0)  // 找到是谁可以填
+				break;
+		if (!modifyElement(pos, i))
+			return false;
+	}
+	return true;
+}
+inline bool Core::modifyElement(int pos, int r) {
+	int p = pos / 9;  // 行号
+	int q = pos % 9;  // 列号
+	int h;
+	x[pos] = r;
+	rep(i, 1, 9)
+		a[pos][i] = 1;
+	a[pos][0] = 8;  // 代表只有一个元素可以填
+	a[pos][r] = 0;  // 那就是r
+	rep(j, 0, 8) {  // 增加同行的其它格子的限制
+		h = Position(p, j);
+		if (h != pos && !deleteElement(h, r))
+			return false;
+	}
+	rep(i, 0, 8) {  // 增加同列的其它格子的限制
+		h = Position(i, q);
+		if (h != pos && !deleteElement(h, r))
+			return false;
+	}
+	rep(i, 0, 2)  // 增加同九宫格的其它格子的限制
+		rep(j, 0, 2) {
+		h = (3 * (p / 3) + i) * 9 + (3 * (q / 3) + j);
+		if (h != pos && !deleteElement(h, r))
+			return false;
+	}
+	return true;
+}
+inline void Core::out_file(int *s) {
+
+	char ans[300];
+	int len = 0;
+	rep(i, 0, 80) {
+		if (s[i] >= 0 && s[i] <= 9)
+			s[i] += '0';
+		ans[len++] = s[i];
+		ans[len++] = (((i + 1) % 9) == 0) ? '\n' : ' ';
+		if (debug) {
+			printf("%c ", s[i]);
+			if ((i + 1) % 9 == 0)
+				printf("\n");
+		}
+	}
+	if (debug)
+		printf("\n");
+
+	ans[len++] = '\n';
+	ans[len] = '\0';
+	// ans[len] = '\0';  // fputs(ans, out); // fputs("\n", out);
+	fwrite(ans, 1, strlen(ans), out);
+	// ans[len] = '\0'; //fprintf(out, "%s\n", ans);
+}
+inline void Core::dfs2(int k, int type) {
+	int mx = -1, mxi;
+	rep(i, 0, 80) {
+		if (a[i][0]<8 && a[i][0]>mx) {
+			mx = a[i][0];
+			mxi = i;
+		}
+	}
+	if (mx == -1) {
+		rep(i, 0, 80) {
+			if (x[i] <= 9)
+				x[i] += '0';
+		}
+		if (hasAnswer == 0)
+		{
+			//cout << "*********" << endl;
+			rep(i, 0, 80)
+				ans_first[i] = x[i] - '0';
+		}
+		if (type == 1)
+			out_file(x);
+		
+		hasAnswer++;//hasAnswer = true;
+		return ;
+	}
+	memcpy(a_backup[k], a, sizeof(a));
+	rep(i, 1, 9) {
+		if (a[mxi][i] == 0) {
+			if (modifyElement(mxi, i))
+				dfs2(k + 1, type);
+			memcpy(a, a_backup[k], sizeof(a));
+		}
+		if (hasAnswer >= 2)
+			return;
+	}
+}
+inline void Core::dfs1(int k,int type) {
+	if (hasAnswer)
+		return;
+	int x, y, h;
+	x = k / 9;
+	y = k % 9;
+	h = belonging(x, y);
+	if (k > 80) {
+		if (type == 1)
+			out_file(aaa);
+		ans_all_cnt++;
+
+		memcpy(ans_all[ans_all_cnt], aaa, sizeof(aaa));//存储答案
+
+		if (++cnt >= value) {
+			hasAnswer = 1;//true
+			// exit(0);
+		}
+		return;
+	}
+	rep(i, 1, 9) {
+		if (find(x, y, i)) {
+			aaa[k] = i + '0';
+
+			vis_row[x][i] = vis_col[y][i] = vis_magic[h][i] = true;
+			dfs1(k + 1, type);
+			vis_row[x][i] = vis_col[y][i] = vis_magic[h][i] = false;
+		}
+	}
+}
+
+inline int Core::freedom(int a[M]) {
+	int res = 0, x, y, h;
+	rep(node, 0, 80) {
+		if (a[node] != 0)
+			continue;
+		x = node / 9;
+		y = node % 9;
+		rep(q, 0, 8)
+			if (q != y && a[x * 9 + q] == 0)
+				res++;
+		rep(p, 0, 8)
+			if (p != x && a[p * 9 + y] == 0)
+				res++;
+		rep(i, 0, 2)  // 增加同九宫格的其它格子的限制
+			rep(j, 0, 2) {
+			h = (3 * (x / 3) + i) * 9 + (3 * (y / 3) + j);
+			if (h != node && a[h] == 0)
+				res++;
+		}
+	}
+	return res;
+}
+
+inline double Core::getRandData(int min, int max)
+{
+	double m1 = (double)(rand() % 101) / 101;                        // 计算 0，1之间的随机小数,得到的值域近似为(0,1)
+	min++;                                                                             //将 区间变为(min+1,max),
+	double m2 = (double)((rand() % (max - min + 1)) + min);    //计算 min+1,max 之间的随机整数，得到的值域为[min+1,max]
+	m2 = m2 - 1;                                                                        //令值域为[min,max-1]
+	return m1 + m2;                                                                //返回值域为(min,max),为所求随机浮点数
+}
+
+inline void Core::dfs3(int k, int tot) {
+	if (k > 80)
+		return;
+	double t = getRandData(0, 1);
+	if ((tot == num_0 || t >= p_exist) && (80 - k + 1 >= num_0 - tot + 1))
+	{
+		game[k] = ans_all[id][k];
+		dfs3(k + 1, tot);
+	}
+	else {
+		//square[belonging(k / 9, k % 9)]++;
+		dfs3(k + 1, tot + 1);
+	}
+}
+
+inline int Core::work3(int num_0_t, int id_t, double p_t) {
+	//freopen("Ans.txt", "w", stdout);
+	memset(isEmpty, true, sizeof(isEmpty));
+	id = id_t;
+	p_exist = p_t;
+	num_0 = num_0_t;
+	//while (true) 
+	{
+		memset(game, 0, sizeof(game));
+		//memset(square, 0, sizeof(square));
+		dfs3(0, 0);
+		//if (find()) 
+		{
+			rep(i, 0, 80) {
+				if (game[i]) {
+					game[i] -= '0';
+					isEmpty[i] = false;
+				}
+				if (debug)
+					cout << game[i] << " ";
+				//cout << (ans_all[id][i] - '0') << " ";
+				if (((i + 1) % 9) == 0 && debug)
+						cout << endl;
+			}
+			//cout << "###########################" << endl;
+			//return;
+		}
+	}
+
+	int fre = freedom(game);
+	if (debug)
+		cout << "自由度： " << fre << endl;
+	//cout << "-----------" << endl;
+	//cout << "___________" << endl;
+	return fre;
+}
