0f701181bb7a663ef25328a4deb4aa5ad1769e3a
ohazyi_ACT <291045048@qq.com>
2017-10-08 22:46:10 +0800

    Generate DLL: with SudokuDLL.dll and *.lib

--- TestDLL/TestDLL/Main.cpp
+++ TestDLL/TestDLL/Main.cpp
@@ -3,6 +3,17 @@
 int main() {
 	Core c;
 	int a[11][M];
-	c.generate(10, 1, a);
-
+	c.generate(10, 55, 55, true, a);
+	printf("%d\n", c.debug);
+	//c.init_gen(1, 0);
+	rep(i, 1, 10) {
+		cout << "-----------------------" << endl;
+		rep(j, 0, 80) {
+			cout << a[i][j] << " ";
+			if ((j + 1) % 9 == 0)
+				cout << endl;
+		}
+		//cout << "-----------------------" << endl;
+	}
+	printf("%d\n",c.find(1, 1, 1));
 }
